186a2b7a0380f28668f35461fa14af45dd28ff82
Tiyashaghosh <149707689+Tiyashaghosh@users.noreply.github.com>
2025-06-27 11:53:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import imaplib
+import email
+
+my_email = "tiyasha5189@gmail.com"
+my_password = "nvregdpszbmbxinv"
+imap_server = "imap.gmail.com"
+
+
+with imaplib.IMAP4_SSL(imap_server) as imap:
+    imap.login(user= my_email,password=my_password)
+    imap.select("inbox")
+    _,msgnums = imap.search(None,"ALL") # here "_" signifies the status if the code ran successfully
+
+    for msgnum in msgnums[0].split(): # here we get the data as [b '1 2 3 4'] so need to spilt it
+        _,data = imap.fetch(msgnum,"(RFC822)")
+        message = email.message_from_bytes(data[0][1])
+
+        print(f"Message Number: {msgnum}")
+        print(f"From : {message.get('From')}")
+        print(f"To : {message.get('To')}")
+        print(f"Bcc : {message.get('Bcc')}")
+        print(f"Date : {message.get('Date')}")
+        print(f"Subject : {message.get('Subject')}")
+        print("\n")
+
+        print("Contents:")
+        for part in message.walk():
+            if part.get_content_type() == "text/plain":
+                print(part.as_string())
